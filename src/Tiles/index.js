c818170997ab237cb05dcec0c2121c9b0e92e227
Jaime Scowcroft <jaime.scowcroft@stockport.gov.uk>
2021-03-01 15:16:09 +0000

    key

--- src/Tiles/index.js
+++ src/Tiles/index.js
@@ -2,7 +2,7 @@ import Leaflet from 'leaflet'
 import mapboxGL from 'mapbox-gl-leaflet' // eslint-disable-line no-unused-vars
 
 const token =
-'pk.eyJ1IjoiZ2lzLXN0b2NrcG9ydCIsImEiOiJja2xrcmlxcm8yNTBqMnVwcmplc3BneHFuIn0.KcusBgcrjzWgHVAKmx3Wkw'
+'pk.eyJ1IjoiZ2lzLXN0b2NrcG9ydCIsImEiOiJja2xxcTUzZGUxZml1MnFxeTNidmt5c3kyIn0.QOsaZ7uPj4Kv2lwL63Crlg'
 
 const greyscale = Leaflet.mapboxGL({
   style: 'mapbox://styles/gis-stockport/ck5gr2oav0utc1ipbdkcjnjop',
